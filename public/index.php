225efc7c3fa1bf4088d2bdfeae36f355df4febe7
Eduardo Cansian Rodrigues <144130276+EduardoCansian@users.noreply.github.com>
2025-05-23 21:52:15 +0000

    Configura ambiente de desenvolvimento PHP para Codespaces

--- public/index.php
+++ public/index.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Teste PHP no Codespace</title>
-</head>
-<body>
-    <h1>Olá do PHP no Codespace!</h1>
-    <p>A versão do PHP é: <?php echo phpversion(); ?></p>
-    <p>A data atual é: <?php echo date('Y-m-d H:i:s'); ?></p>
-</body>
-</html>
+<?php
 
+const NOME_CURSO = "Sistemas de Informação";
+const INSTITUICAO = "Multivix";
+
+$semestre_atual = 7;
+
+echo "Estou no" . $semestre_atual . "° período do curso de"  . NOME_CURSO . "na" . INSTITUICAO;
+
+?>
